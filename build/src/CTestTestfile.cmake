d27867e64cd194ff110ea4137a2be3b55f944363
Rami-Slicer <rami.slicer@gmail.com>
2021-04-07 14:57:49 -0500

    remove stuff

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/reese/code/src/voxelite/src
-# Build directory: /home/reese/code/src/voxelite/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
